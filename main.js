dcb32c64c524543dfb3f85373ece2807556e1a3d
unknown <abdielmamani@upeu.edu.pe>
2024-05-16 20:02:05 -0500

    last change

--- main.js
+++ main.js
@@ -3,24 +3,45 @@ function playSound(elemt_Audio){
     console.log(elemt_Audio);
     
 }
-
-
 const keyList =  document.querySelectorAll('.key');
 
 let contador=0;
 
-while(contador < 9){
+for(contador =0; contador<keyList.length; contador++){
     const key = keyList[contador];
-    // console.log(key);
     const instrument = key.classList[1];
-    console.log(instrument);
-    const idAudio = `#sonido_${instrument}`;
-    console.log(idAudio);
 
-    key.onclick = function (){
+    const idAudio = `#sonido_${instrument}`;
+
+    key.onclick = function(){
         playSound(idAudio);
     };
-    contador++;
+
+    key.onkeydown = function (event){
+        console.log(event);
+        if(event.code === 'Space' || event.code === 'Enter'){
+            key.classList.add('activa');
+        }
+    } 
+    key.onkeyup = function(){
+        key.classList.remove('activa');
+    }
 }
+
+
+// while(contador < 9){
+//     const key = keyList[contador];
+//     // console.log(key);
+//     const instrument = key.classList[1];
+//     console.log(instrument);
+//     const idAudio = `#sonido_${instrument}`;
+//     console.log(idAudio);
+
+//     key.onclick = function (){
+//         playSound(idAudio);
+//     };
+//     contador++;
+// }
+
 
 
